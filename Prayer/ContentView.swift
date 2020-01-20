962164d400d90f2bdd98dfe5200cd9d7e42662a4
Christopher Davis <chris@christopheralandavis.com>
2020-01-20 15:28:33 -0500

    Initial Commit

---
+++ Prayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Prayer
+//
+//  Created by Christopher Davis on 1/20/20.
+//  Copyright © 2020 Christopher Davis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
